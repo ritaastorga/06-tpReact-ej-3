44730b4b443d38a6042c8d7b1fabe1340d8af1b0
rita <ritaastorga10@gmail.com>
2023-06-01 21:53:28 -0300

    realicé el tercer ejercicio del tp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Mensaje from "./components/Mensaje";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Mensaje textoMensaje="my friend"></Mensaje>
+    </>
+  );
 }
 
 export default App;
